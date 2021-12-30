df064f61fdd53add9485c8c639f9e1be08389492
SirvanD <sirvansh@gmail.com>
2021-12-30 21:11:48 +1100

    bugs fixed

--- key_warmups/count_blanks.js
+++ key_warmups/count_blanks.js
@@ -1,12 +1,42 @@
 const countBlanks = (arr) => {
   let total = 0;
+  let alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let strArray = [];
   arr.forEach((str) => {
-    if (str == "") {
-      total += 1;
-    }
+    strArray = str.split("");
+    if (alphabet.includes(strArray[0])) {
+    } else total += 1;
   });
   return total;
 };
 
-countBlanks(["cake", "", "pudding", ""]); // => 2
+countBlanks(["cake", " ", "pudding", ""]); // => 2
 countBlanks(["cake"]); // => 0
+countBlanks(["", " ", "   "]); // => 3
