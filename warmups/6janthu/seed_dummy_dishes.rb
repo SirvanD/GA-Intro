bc3cb6808834361a0912cd03113bf13c6009598c
SirvanD <sirvansh@gmail.com>
2022-01-06 10:34:04 +1100

    ruby

---
+++ warmups/6janthu/seed_dummy_dishes.rb
@@ -0,0 +1,19 @@
+require 'pg'
+
+dish_names = ["Arepas", "Barbecue Ribs", "Bruschette with Tomato", "Bunny Chow", "Caesar Salad", "California Maki", "Caprese Salad", "Cauliflower Penne", "Cheeseburger", "Chicken Fajitas", "Chicken Milanese", "Chicken Parm", "Chicken Wings", "Chilli con Carne", "Ebiten maki", "Fettuccine Alfredo", "Fish and Chips", "French Fries", "Sausages", "French Toast", "Hummus", "Katsu Curry", "Kebab", "Lasagne", "Linguine with Clams", "Massaman Curry", "Meatballs with Sauce", "Mushroom Risotto", "Pappardelle alla Bolognese", "Pasta Carbonara", "Pasta and Beans", "Pasta with Tomato and Basil", "Peking Duck", "Philadelphia Maki", "Pho", "Pierogi", "Pizza", "Poke", "Pork Belly Buns", "Pork Sausage Roll", "Poutine", "Ricotta Stuffed Ravioli", "Risotto with Seafood", "Salmon Nigiri", "Scotch Eggs", "Seafood Paella", "Som Tam", "Souvlaki", "Stinky Tofu", "Sushi", "Tacos", "Teriyaki Chicken Donburi", "Tiramisù", "Tuna Sashimi", "Vegetable Soup"]
+
+
+conn = PG.connect(dbname: 'goodfoodhunting')
+
+20.times {
+
+    random_name = dish_names.sample
+    random_image_url = "https://baconmockup.com/#{rand(200..400)}/#{rand(200.300)}"
+    sql = "INSERT INTO dishes (name,image_url) values ($1,$2);"
+    conn.exec_params(sql, [random_name, random_image_url])
+}
+
+conn.close
+
+# puts 'psql goodfoodhunting'
+# puts 'SELECT * FROM dishes'
