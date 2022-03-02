9600854b54c50d84631993f9c73ca87065dabc66
SirvanD <sirvansh@gmail.com>
2022-03-02 13:48:16 +1100

    challenge

---
+++ code_challenges/HighestAndLowest.js
@@ -0,0 +1,11 @@
+const highAndLow = (numbers) => {
+  var array = numbers.split(" ");
+  const high = Math.max(...array);
+  const low = Math.min(...array);
+  return `${high} ${low}`;
+};
+
+console.log(highAndLow("1 2 3 4 5")); // return "5 1"
+console.log(highAndLow("1 2 -3 4 5")); // return "5 -3"
+console.log(highAndLow("1 9 3 4 -5")); // return "9 -5"
+console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
