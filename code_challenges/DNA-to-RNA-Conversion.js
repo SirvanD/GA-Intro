a0724a716e24125b2fa77bf6ccfbfd221030dbef
SirvanD <sirvansh@gmail.com>
2022-05-30 12:45:00 +1000

    daily challenge

--- code_challenges/DNA-to-RNA-Conversion.js
+++ code_challenges/DNA-to-RNA-Conversion.js
@@ -9,9 +9,22 @@
 // "GCAT"  =>  "GCAU"
 // The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
 
-const DNAtoRNA = (str) => {
-  return str.replace(/T/g, "U");
-};
+// const DNAtoRNA = (str) => {
+//   return str.replace(/T/g, "U");
+// };
+
+function DNAtoRNA(dna) {
+  return dna
+    .split("")
+    .map((char) => {
+      if (char === "T") {
+        return "U";
+      } else {
+        return char;
+      }
+    })
+    .join("");
+}
 
 console.log(DNAtoRNA("GCAT"));
 console.log(DNAtoRNA("GCAR"));
