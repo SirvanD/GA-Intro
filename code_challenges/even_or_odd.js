08cfa7a0c3d4f00fddf029b50ca0dda94cb373b5
SirvanD <sirvansh@gmail.com>
2022-06-05 20:18:55 +1000

    daily challenge

--- code_challenges/even_or_odd.js
+++ code_challenges/even_or_odd.js
@@ -3,7 +3,7 @@ function even_or_odd(number) {
   number % 2 === 0 ? (result = "Even") : (result = "Odd");
   return result;
 }
-
+//comment daily challenge
 console.log(even_or_odd(25));
 console.log(even_or_odd(14));
 console.log(even_or_odd(3));
