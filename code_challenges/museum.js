56d836d19762106b9b90cc5382f737c760b50e8a
SirvanD <sirvansh@gmail.com>
2022-06-27 16:22:58 +1000

    daily challenge

--- code_challenges/museum.js
+++ code_challenges/museum.js
@@ -12,3 +12,22 @@
 // * Input: [1,2,3,4,5], output= [2,3,4,5]
 // * Input: [5,3,2,1,4], output = [5,3,2,4]
 // * Input: [2,2,1,2,1], output = [2,2,2,1]
+
+const removeSmallest = (numbers) => {
+  // let sortedArray = [];
+  // sortedArray = numbers.sort((a, b) => a - b);
+  if (numbers.length === 0) {
+    return [];
+  }
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] === Math.min(...numbers)) {
+      let smallest = numbers.splice(i, 1);
+      return numbers;
+    }
+  }
+};
+
+console.log(removeSmallest([1, 2, 3, 4, 5]));
+console.log(removeSmallest([5, 3, 2, 1, 4]));
+console.log(removeSmallest([2, 2, 1, 2, 1]));
+console.log(removeSmallest([]));
